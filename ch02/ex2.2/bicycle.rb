2e0b20427327b3ebaaddb5fa9018781b74d0036b
ku00 <skentaro36@gmail.com>
2018-01-13 21:01:21 +0900

    [ch02] 単一の責任を持つクラスをつくる

---
+++ ch02/ex2.2/bicycle.rb
@@ -0,0 +1,9 @@
+chainring = 52
+cog = 11
+ratio = chainring / cog.to_f
+p ratio
+
+chainring = 30
+cog = 27
+ratio = chainring / cog.to_f
+p ratio
---
+++ ch02/ex2.2/gear-2.rb
@@ -0,0 +1,24 @@
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    # タイヤはリムのまわりを囲むので、直径を計算するためには2倍する
+    ratio * (rim + (tire*2))
+  end
+end
+
+p Gear.new(52, 11, 26, 1.5).gear_inches
+p Gear.new(52, 11, 24, 1.25).gear_inches
+
+p Gear.new(52, 11).ratio
