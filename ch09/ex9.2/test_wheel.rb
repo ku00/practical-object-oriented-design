f39a1c61b82b15796262b0f733d6ef560beb590d
ku00 <skentaro36@gmail.com>
2018-02-06 23:24:16 +0900

    [ch09_9.2_*] テストを使ってロールを文書化する

--- ch09/ex9.2/test_wheel.rb
+++ ch09/ex9.2/test_wheel.rb
@@ -2,8 +2,15 @@ require 'minitest/autorun'
 require_relative 'wheel'
 
 class TestWheel < Minitest::Test
+  def setup
+    @wheel = Wheel.new(26, 1.5)
+  end
+
+  def test_implements_the_diameterizable_interface
+    assert_respond_to(@wheel, :diameter)
+  end
+
   def test_calculates_diameter
-    wheel = Wheel.new(26, 1.5)
-    assert_in_delta(29, wheel.diameter, 0.01)
+    assert_in_delta(29, @wheel.diameter, 0.01)
   end
 end
