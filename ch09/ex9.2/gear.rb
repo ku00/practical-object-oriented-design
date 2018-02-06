44274cecf947af3e688201d8053a40a613d9f926
ku00 <skentaro36@gmail.com>
2018-02-06 23:13:28 +0900

    [ch09_9.2_*] テスト対象のオブジェクトを隔離する

--- ch09/ex9.2/gear.rb
+++ ch09/ex9.2/gear.rb
@@ -1,17 +1,16 @@
 require_relative 'wheel'
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
   def initialize(args)
     @chainring = args[:chainring]
     @cog = args[:cog]
-    @rim = args[:rim]
-    @tire = args[:tire]
+    @wheel = args[:wheel]
   end
 
   def gear_inches
-    ratio * Wheel.new(rim, tire).diameter
+    ratio * wheel.diameter
   end
 
   def ratio
--- ch09/ex9.2/test_gear.rb
+++ ch09/ex9.2/test_gear.rb
@@ -1,13 +1,13 @@
 require 'minitest/autorun'
 require_relative 'gear'
+require_relative 'wheel'
 
 class TestGear < Minitest::Test
   def test_calculates_gear_inches
     gear = Gear.new(
       chainring: 52,
       cog: 11,
-      rim: 26,
-      tire: 1.5
+      wheel: Wheel.new(26, 1.5)
     )
     assert_in_delta(137.1, gear.gear_inches, 0.01)
   end
