9c0f7658b1d32c28dc18c8b0d48992d9d7f5e314
ku00 <skentaro36@gmail.com>
2018-02-10 12:00:22 +0900

    [ch09_9.6] 抽象スーパークラスの振る舞いをテストする

--- ch09/ex9.6/test_bicycle.rb
+++ ch09/ex9.6/test_bicycle.rb
@@ -1,12 +1,46 @@
 require 'minitest/autorun'
 require_relative 'bicycle'
-require_relative 'road_bike'
 require_relative 'test_bicycle_interface'
+require_relative 'test_bicycle_subclass'
+
+class StubbedBike < Bicycle
+  def default_tire_size
+    0
+  end
+
+  def local_spares
+    { saddle: 'painful' }
+  end
+end
+
+class TestStubbedBike < MiniTest::Test
+  include TestBicycleSubclass
+
+  def setup
+    @object = StubbedBike.new
+  end
+end
 
 class TestBicycle < MiniTest::Test
   include TestBicycleInterface
 
   def setup
     @bike = @object = Bicycle.new({ tire_size: 0 })
+    @stubbed_bike = StubbedBike.new
+  end
+
+  def test_forces_subclasses_to_implement_default_tire_size
+    assert_raises(NotImplementedError) { @bike.default_tire_size }
+  end
+
+  def test_includes_local_spares_in_spares
+    assert_equal(
+      @stubbed_bike.spares,
+      {
+        tire_size: 0,
+        chain: '10-speed',
+        saddle: 'painful'
+      }
+    )
   end
 end
--- ch09/ex9.6/test_road_bike.rb
+++ ch09/ex9.6/test_road_bike.rb
@@ -8,6 +8,10 @@ class TestRoadBike < MiniTest::Test
   include TestBicycleSubclass
 
   def setup
-    @road_bike = @object = RoadBike.new
+    @road_bike = @object = RoadBike.new(tape_color: 'red')
+  end
+
+  def test_puts_tape_color_in_local_spares
+    assert_equal('red', @road_bike.local_spares[:tape_color])
   end
 end
